7ecd81eba841a9b18fa4ac9c67b3ce2f730197ad
unknown <makakpo@markom-bs.com>
2023-12-06 13:44:16 -0500

    La version terminee de mon projet personnel

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const cards = document.querySelectorAll('.memory-card');
 
+const nbreDeCoups = document.getElementById('coups');//déclaration de ma constante
+//console.log(nbreDeCoups);
+
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let nbCoups = 0;//Je déclare ma vaiable dans laquelle je mettrai le nombre de coups
+
 
 function flipCard() {
   if (lockBoard) return;
@@ -24,26 +29,25 @@ function flipCard() {
   checkForMatch();
 }
 
+//la fonction qui vérifie si les 2 cartes sont identifiées
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
+  nbCoups = nbCoups + 1;//quand le joueur clique sur une première carte, puis une deuxième carte, le compteur augmente de 1
+  nbreDeCoups.innerText = `Nombre de coups jouer égale à ${nbCoups}`;//J'ajoute du texte avec innerText 
   isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-
   resetBoard();
 }
 
 function unflipCards() {
   lockBoard = true;
-
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-
     resetBoard();
   }, 1500);
 }
@@ -61,3 +65,13 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+//Mon code JavaScript pour rafraichir la page en vue d'une nouvelle partie.
+const reprendre = document.getElementById('reprendreJeu');//déclaration de la constante pour sélectionner le id
+//j'ajoute la fonction qui permet de rafraichir la page
+function reprendreLeJeu (){
+  location.reload();
+}
+reprendre.addEventListener('click', reprendreLeJeu);//j'ajoute un évenement click sur ma fonction reprendreLeJeu
+//console.log(reprendre);
+
